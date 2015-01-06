0b5269ee9831dd2819e0ea38945b3c1a7fd3edd2
Brandon Harris <brandon@electricimp.com>
2015-01-05 16:10:42 -0800

    Adding Avatar for Peter.

--- application.js
+++ application.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
   var roomNamesById = {};
   var tagNamesById = {};
   var colorsById = {};
-  var avtrImgByName = { "Mr. Penguin" : 'css/img/avtr_penguin.png',
+  var avtrImgByName = { "Peter" : 'css/img/peter_icon.png',
                      "Brionna" : 'css/img/avtr_f_wt_shirt.png',
                      "Brandon" : 'css/img/avtr_m_gr_swtr.png',
                      "Gino" : 'css/img/avtr_m_bl_suit.png',
